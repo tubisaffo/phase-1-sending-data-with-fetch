f51f8debf9e13e303f96c1af226cbcb6eadecb73
tubisaffo <tubisaffo@gmail.com>
2024-04-15 17:41:56 +0300

    POST request

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+const submitData = (name, email) => {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((response) => response.json())
+    .then(({ id }) => (document.body.innerHTML = id))
+    .catch(({ message }) => (document.body.innerHTML = message));
+};
